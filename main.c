dcba5071952249d176893a8f00d26ac26765907a
DICKY IBROHIM <dicky.director@gmail.com>
2019-06-15 02:13:31 +0700

    First Upload
    First Upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+// DICKY IBROHIM
+main ()
+{
+    int input, a, i=1;
+    int result;
+    printf("Input=");
+    scanf ("%d",&input);
+
+for (a=1;a<input;a++)
+{
+i*=a;
+printf("%d*",a);
+
+}
+i*=a;
+printf("%d=%d",a,i);
+
+}
+
+
